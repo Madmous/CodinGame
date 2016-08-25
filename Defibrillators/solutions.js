cc1a670e423d9c0d4df63adb9f73162040425528
Moustapha A Diouf <mail@madmous.com>
2016-08-25 22:43:06 +0200

    pass 1st 2 tests

--- Defibrillators/solutions.js
+++ Defibrillators/solutions.js
@@ -6,11 +6,35 @@
 var LON = readline();
 var LAT = readline();
 var N = parseInt(readline());
+
+var res = {};
+var userCoordinate = {
+    lon: parseFloat(LON.replace(",", ".")),
+    lat: parseFloat(LAT.replace(",", "."))
+};
+
 for (var i = 0; i < N; i++) {
     var DEFIB = readline();
+    var defibAfterSplit = DEFIB.split(';');
+
+    var defibCoordinate = {
+        lon: parseFloat(defibAfterSplit[4].replace(",", ".")),
+        lat: parseFloat(defibAfterSplit[5].replace(",", "."))
+    };
+    var distance = calculateDistance(defibCoordinate, userCoordinate);
+    if (res.distance === undefined) {
+        res.distance = distance;
+        res.name = defibAfterSplit[1];
+    } else if (res.distance > distance) {
+        res.distance = distance;
+        res.name = defibAfterSplit[1];
+    }
 }
+print(res.name);
 
-// Write an action using print()
-// To debug: printErr('Debug messages...');
-
-print('answer');
+function calculateDistance(defibCoordinate, userCoordinate) {
+    var x = (defibCoordinate.lon - userCoordinate.lon) * 
+                Math.cos(defibCoordinate.lat + userCoordinate.lat) / 2;
+    var y = defibCoordinate.lat - userCoordinate.lat;
+    return Math.sqrt(Math.abs(x+y)) * 6371;
+}
