e31dee175bd7877a0f24d8ad7dd59f258a51fc73
Moustapha A Diouf <mail@madmous.com>
2016-09-06 21:29:03 +0200

    initial commit

---
+++ HorseRacingDuals/solutions.js
@@ -0,0 +1,14 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+var N = parseInt(readline());
+for (var i = 0; i < N; i++) {
+    var pi = parseInt(readline());
+}
+
+// Write an action using print()
+// To debug: printErr('Debug messages...');
+
+print('answer');
