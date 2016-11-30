db6ccac21c2bf241755a19d37ac6d7e94f9ab799
Moustapha A Diouf <mail@madmous.com>
2016-11-30 22:10:30 +0100

    Pass all tests

--- SkynetRevolutionEpisode1/solution.js
+++ SkynetRevolutionEpisode1/solution.js
@@ -1,73 +1,76 @@
-var inputs = readline().split(' ');
-var N = parseInt(inputs[0]); // the total number of nodes in the level, including the gateways
-var L = parseInt(inputs[1]); // the number of links
-var E = parseInt(inputs[2]); // the number of exit gateways
+const inputs = readline().split(' ');
 
-var linkBetweenNodes = [];
-var gatewayNodes = [];
+const N = parseInt(inputs[0]);
+const L = parseInt(inputs[1]);
+const E = parseInt(inputs[2]);
 
-for (var i = 0; i < L; i++) {
-    var inputs = readline().split(' ');
-    
-    linkBetweenNodes.push(inputs);
-}
+let linkBetweenNodes = [];
+let gatewayNodes     = [];
 
-for (var i = 0; i < E; i++) {
-    var EI = parseInt(readline());
-    
-    gatewayNodes.push(EI);
-}
+const parseElement = function(element) {
+    return {
+        N1 : parseInt(element[0]),
+        N2 : parseInt(element[1])
+    };
+};
 
-while (true) {
-    var SI = parseInt(readline());
-    
-    var linksWithAgent = nodesLinkedWithAgent(SI);
-    var agentGateway = agentGatewayLink(linksWithAgent);
-    
-    if (agentGateway.length > 0){
-        print(agentGateway[0]);
-    } else {
-        var N1 = parseInt(linksWithAgent[0][0]);
-        var N2 = parseInt(linksWithAgent[0][1]);
-        
-        print(N1 + ' ' + N2);
-    }
-}
-
-function nodesLinkedWithAgent(SI) {
-    var links = [];
+const nodesLinkedWithAgent = function(SI) {
+    let links = [];
     
     linkBetweenNodes.forEach((element) => {
-        var N1 = parseInt(element[0]);
-        var N2 = parseInt(element[1]);
+        const {N1, N2} = parseElement(element);
         
         if (N1 === SI || N2 === SI) {
             links.push(element);
         }
     });
     
-    printErr(links);
-    
     return links;
-}
+};
 
-function agentGatewayLink(linksWithAgent) {
-    var agentGatewayLink = [];
+ const getAgentGatewayLinks = function(linksWithAgent) {
+    let agentGatewayLink = [];
     
     linksWithAgent.forEach((element) => {
-        var N1 = parseInt(element[0]);
-        var N2 = parseInt(element[1]);
+        const {N1, N2} = parseElement(element);
         
-        if (gatewayNodes.indexOf(N1) === 0  ||
-            gatewayNodes.indexOf(N2) === 0) {
+        if (gatewayNodes.indexOf(N1) !== -1  ||
+            gatewayNodes.indexOf(N2) !== -1) {
+                
+            printErr(element);
                 
             agentGatewayLink.push(N1 + ' ' + N2);
         }
     });
 
     return agentGatewayLink;
+};
+
+for (let i = 0; i < L; i++) {
+    const inputs = readline().split(' ');
+    
+    linkBetweenNodes.push(inputs);
 }
 
+for (let i = 0; i < E; i++) {
+    const EI = parseInt(readline());
+    
+    gatewayNodes.push(EI);
+}
+
+while (true) {
+    const SI = parseInt(readline());
+    const linksWithAgent = nodesLinkedWithAgent(SI);
+    const agentGatewayLink = getAgentGatewayLinks(linksWithAgent);
+    
+    if (agentGatewayLink.length > 0){
+        print(agentGatewayLink[0]);
+    } else {
+        const {N1, N2} = parseElement(linksWithAgent[0]);
+        
+        print(N1 + ' ' + N2);
+    }
+}
 
 
 
