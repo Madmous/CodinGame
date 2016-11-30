a2a6b1907cdd348269e2d9aca450ea3feb48fabd
Moustapha A Diouf <mail@madmous.com>
2016-11-30 21:40:57 +0100

    Pass tests 02 and 03

--- SkynetRevolutionEpisode1/solution.js
+++ SkynetRevolutionEpisode1/solution.js
@@ -1,8 +1,3 @@
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
 var inputs = readline().split(' ');
 var N = parseInt(inputs[0]); // the total number of nodes in the level, including the gateways
 var L = parseInt(inputs[1]); // the number of links
@@ -15,49 +10,64 @@ for (var i = 0; i < L; i++) {
     var inputs = readline().split(' ');
     
     linkBetweenNodes.push(inputs);
-    //var N1 = parseInt(inputs[0]); // N1 and N2 defines a link between these nodes
-    //var N2 = parseInt(inputs[1]);
 }
 
 for (var i = 0; i < E; i++) {
-    var EI = parseInt(readline()); // the index of a gateway node
+    var EI = parseInt(readline());
+    
     gatewayNodes.push(EI);
 }
 
-// game loop
 while (true) {
-    var SI = parseInt(readline()); // The index of the node on which the Skynet agent is positioned this turn
-
-    var linksWithGateway = nodesLinkedWithGateway(SI);
+    var SI = parseInt(readline());
     
-    for (var i = 0 ; i < linksWithGateway.length ; i++) {
-        print(linksWithGateway[i]);
+    var linksWithAgent = nodesLinkedWithAgent(SI);
+    var agentGateway = agentGatewayLink(linksWithAgent);
+    
+    if (agentGateway.length > 0){
+        print(agentGateway[0]);
+    } else {
+        var N1 = parseInt(linksWithAgent[0][0]);
+        var N2 = parseInt(linksWithAgent[0][1]);
+        
+        print(N1 + ' ' + N2);
     }
-    //print(SI + ' ' + gatewayNodes);
-    
-    // Write an action using print()
-    // To debug: printErr('Debug messages...');
-
-
-    // Example: 0 1 are the indices of the nodes you wish to sever the link between
-    //print('0 1');
 }
 
-function nodesLinkedWithGateway(SI) {
+function nodesLinkedWithAgent(SI) {
     var links = [];
     
-    for (var index in linkBetweenNodes) {
-        var N1 = parseInt(linkBetweenNodes[index][0]);
-        var N2 = parseInt(linkBetweenNodes[index][1]);
+    linkBetweenNodes.forEach((element) => {
+        var N1 = parseInt(element[0]);
+        var N2 = parseInt(element[1]);
         
-        printErr(N1);
-        printErr(N2);
-        printErr(gatewayNodes[0]);
-        printErr(N1 === gatewayNodes[0]);
-        
-        if (N1 === gatewayNodes[0] || N2 === gatewayNodes[0] ) {
-            links.push(N1 + ' ' + N2);
+        if (N1 === SI || N2 === SI) {
+            links.push(element);
         }
-    }
+    });
+    
+    printErr(links);
+    
     return links;
 }
+
+function agentGatewayLink(linksWithAgent) {
+    var agentGatewayLink = [];
+    
+    linksWithAgent.forEach((element) => {
+        var N1 = parseInt(element[0]);
+        var N2 = parseInt(element[1]);
+        
+        if (gatewayNodes.indexOf(N1) === 0  ||
+            gatewayNodes.indexOf(N2) === 0) {
+                
+            agentGatewayLink.push(N1 + ' ' + N2);
+        }
+    });
+
+    return agentGatewayLink;
+}
+
+
+
+
